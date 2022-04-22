c081095c7e77cbb8421242cba2fbd0fd0e8847d7
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-04-22 00:02:57 -0300

    primeira parte do gerador de despesas
    gerador de despesas atualmente estou com um problema de que o codigo na aceita meu "onclick" tentando resolver

---
+++ app.js
@@ -0,0 +1,141 @@
+// criando o objeto de onde será extraido o valor
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+    this.ano = ano
+    this.mes = mes
+    this.dia = tipo
+    this.descricao = descricao
+    this.valor = valor
+    }
+
+    //verificando se dados sao validos 
+    validarDados() {
+        for(let i in this) {
+           if(this[i] == undefined || this[i]== '' || this[i]== null) {
+                return false 
+            }
+        }
+        return true
+    }
+
+}
+// gavaçao dos dados no local storage
+    class Bd{
+
+        constructor(){
+            let id = localStorage.getItem('id')
+            if (id === null){
+            localStorage.setItem('id', 0)
+        }
+    }
+        getProximoID(){
+            let proximoId = localStorage.getItem('id')
+           return parseInt(proximoId) + 1 
+        }
+
+        gravar (d){
+           
+
+           let id = this.getProximoID()
+           localStorage.setItem(id, JSON.stringify(d))
+
+           localStorage.setItem('id', id)
+
+    }
+}
+
+let bd = new Bd
+
+// funçao responsavel para extrair o valor dos campos
+
+function cadastrarDespesa() {
+    
+	let ano =  document.getElementById('ano')
+	let mes =  document.getElementById('mes')
+	let dia =  document.getElementById('dia')
+	let tipo =  document.getElementById('tipo')
+	let descricao =  document.getElementById('descricao')
+	let valor =  document.getElementById('valor')
+ 
+ 
+	let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+	if (despesa.validarDados() == true) {
+  
+	 
+   }
+
+  class Despesa {
+	constructor(ano, mes, dia, tipo, descricao, valor) {
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
+
+	validarDados() {
+		for(let i in this) {
+			if(this[i] == undefined || this[i] == '' || this[i] == null) {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+class Bd {
+
+	constructor() {
+		let id = localStorage.getItem('id')
+
+		if(id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
+
+	getProximoId() {
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
+
+	gravar(d) {
+		let id = this.getProximoId()
+
+		localStorage.setItem(id, JSON.stringify(d))
+
+		localStorage.setItem('id', id)
+	}
+}
+
+let bd = new Bd()
+
+
+function cadastro() {
+
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
+
+	let despesa = new Despesa(
+		ano.value, 
+		mes.value, 
+		dia.value, 
+		tipo.value, 
+		descricao.value,
+		valor.value
+	)
+	
+	if(despesa.validarDados()) {
+		bd.gravar(despesa)
+		//dialog de sucesso
+		$('#sucessoGravacao').modal('show') 
+	} else {
+		//dialog de sucesso
+		$('#erroGravacao').modal('show') 
+	}
+}
+}
